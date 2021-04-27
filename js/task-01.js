75e336c84e11138f9fd3ca1fb11781a738a0761c
Dmitriy Shurlyhin <shurlygin91@gmail.com>
2021-04-27 15:12:01 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,22 +3,34 @@ const allCategoryEl = categoryEl.querySelectorAll(`.item`);
 const quantityOfAllCategoriesEl = allCategoryEl.length;
 console.log(`В списке ${quantityOfAllCategoriesEl} категории.`)
 
+allCategoryEl.forEach((element) => 
+    console.log(
+        `Категория: ${element.firstElementChild.textContent}.`,
+        `Количество элементов: ${element.lastElementChild.children.length}`
+)
+)
 
 
-const nameOfCategory = allCategoryEl[0].firstElementChild.textContent;
-console.log(`Категория: ${nameOfCategory}`)
-const quantityOfItems = allCategoryEl[0].querySelectorAll(`li`).length;
-console.log(`Количество элементов: ${quantityOfItems}`)
 
 
 
-const nameOfCategory1 = allCategoryEl[1].firstElementChild.textContent;
-console.log(`Категория: ${nameOfCategory1}`)
-const quantityOfItems1 = allCategoryEl[1].querySelectorAll(`li`).length;
-console.log(`Количество элементов: ${quantityOfItems1}`)
 
 
-const nameOfCategory2 = allCategoryEl[2].firstElementChild.textContent;
-console.log(`Категория: ${nameOfCategory2}`)
-const quantityOfItems2 = allCategoryEl[2].querySelectorAll(`li`).length;
-console.log(`Количество элементов: ${quantityOfItems2}`)
+
+// const nameOfCategory = allCategoryEl[0].firstElementChild.textContent;
+// console.log(`Категория: ${nameOfCategory}`)
+// const quantityOfItems = allCategoryEl[0].querySelectorAll(`li`).length;
+// console.log(`Количество элементов: ${quantityOfItems}`)
+
+
+
+// const nameOfCategory1 = allCategoryEl[1].firstElementChild.textContent;
+// console.log(`Категория: ${nameOfCategory1}`)
+// const quantityOfItems1 = allCategoryEl[1].querySelectorAll(`li`).length;
+// console.log(`Количество элементов: ${quantityOfItems1}`)
+
+
+// const nameOfCategory2 = allCategoryEl[2].firstElementChild.textContent;
+// console.log(`Категория: ${nameOfCategory2}`)
+// const quantityOfItems2 = allCategoryEl[2].querySelectorAll(`li`).length;
+// console.log(`Количество элементов: ${quantityOfItems2}`)
